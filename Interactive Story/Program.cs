1e302b09875d5fd4721ab9c983a0d5381bfc1d8a
Moise Tshibwabwa <moise.tshibwabwa@learnet.se>
2020-10-02 00:40:40 +0200

    Save: Typewrite

--- Interactive Story/Program.cs
+++ Interactive Story/Program.cs
@@ -11,22 +11,30 @@ namespace Interaktive_story
             Console.ForegroundColor
                       = ConsoleColor.Magenta;    //Ger texten en röd färg.
 
-            Console.WriteLine("Vad händer len? Eyy bror, jag har baxat chok mycket parra igår, ska vi beställa kebab? jag är chok hungrig mannen.");
+            Typewrite("Vad händer len? Eyy bror, jag har baxat chok mycket parra igår, ska vi beställa kebab? jag är chok hungrig mannen.");
+            Console.WriteLine();
             Svar1 = Console.ReadLine().ToLower(); //ToLower gör så att man kan svara med stora bokstäver
 
             if (Svar1 == "ja")
             {
-                Console.WriteLine("Vill du han något att dricka?");            //Ifall att man svarar ja kommer programmet gå till static void astaghfirullah
+                Console.WriteLine();
+                Typewrite("Vill du han något att dricka?");
+                Console.WriteLine();        //Ifall att man svarar ja kommer programmet gå till static void astaghfirullah
                 astaghfirullah();
             }
             if (Svar1 == "nej")
             {
+                Console.WriteLine();
+                Typewrite("Yani, är du inte hungrig eller? WALLAH jag ska äta upp allt själv."); //Ifall att man svara nej kommer att svara så här
+                Console.WriteLine();
 
-                Console.WriteLine("Yani, är du inte hungrig eller? WALLAH jag ska äta upp allt själv."); //Ifall att man svara nej kommer att svara så här
             }
             if (Svar1 == "astaghfirullah")
             {
-                Console.WriteLine("Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch");   //Easter egg
+                Console.WriteLine();
+                Typewrite("Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch");   //Easter egg
+                Console.WriteLine();
+
             }
 
             Console.ReadLine();
@@ -38,12 +46,18 @@ namespace Interaktive_story
             Svar2 = Console.ReadLine().ToLower();
             if (Svar2 == "ja")
             {
-                Console.WriteLine("Ait bror, ska jag ta en cola till dig!");
+                Console.WriteLine();
+                Typewrite("Ait bror, ska jag ta en cola till dig!");
+                Console.WriteLine();
+
                 nej();
             }
             if (Svar2 == "nej")
             {
-                Console.WriteLine("Ait, bror men maten kommer om 30 min.");
+                Console.WriteLine();
+                Typewrite("Ait, bror men maten kommer om 30 min.");
+                Console.WriteLine();
+
             }
 
             Console.ReadLine();
@@ -56,13 +70,19 @@ namespace Interaktive_story
 
             if (Svar3 == "nej")
             {
-                Console.WriteLine("Yani, vad vill du dricka? De har bara fanta, cola och 7up.");
+                Console.WriteLine();
+                Typewrite("Yani, vad vill du dricka? De har bara fanta, cola och 7up.");
+                Console.WriteLine();
+
                 fantacola7up();     //Programmet kommer att läsa static void "fantacola7up" om man inte vill ha cola
             }
 
             if (Svar3 == "ja")
             {
-                Console.WriteLine("Ait bror, jag har beställt. Maten är här om 30 minuter.");
+                Console.WriteLine();
+                Typewrite("Ait bror, jag har beställt. Maten är här om 30 minuter.");
+                Console.WriteLine();
+
             }
 
             Console.ReadLine();
@@ -75,18 +95,37 @@ namespace Interaktive_story
             Svar4 = Console.ReadLine().ToLower();
             if (Svar4 == "fanta")
             {
-                Console.WriteLine("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+                Typewrite("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+
             }
             if (Svar4 == "cola")
             {
-                Console.WriteLine("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+                Typewrite("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+
             }
             else if (Svar4 == "7up")
             {
-                Console.WriteLine("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+                Typewrite("Ait bror, jag har beställt. Maten är här om 30 minuter");
+                Console.WriteLine();
+
             }
 
         }
+
+        static void Typewrite(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(60);
+            }
+        }
+
 
     }
 }
